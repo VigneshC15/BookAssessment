a18cf489283823e7f1b0467b33afc9312d4dde53
VigneshC15 <vigneshc1593@gmail.com>
2023-06-11 09:37:32 +0530

    Updates

--- BooksAPI/Controllers/BookAPIController.cs
+++ BooksAPI/Controllers/BookAPIController.cs
@@ -12,7 +12,7 @@ namespace BooksAPI.Controllers
     {
         private readonly IBookService _bookService;
         /// <summary>
-        /// 
+        ///     Constructor
         /// </summary>
         /// <param name="bookService"></param>
         public BookAPIController(IBookService bookService)
@@ -20,37 +20,37 @@ namespace BooksAPI.Controllers
             _bookService = bookService;
         }
         /// <summary>
-        /// 
+        ///     Sort book list by Publisher, Author (last, first), then title
         /// </summary>
-        /// <returns></returns>
+        /// <returns>return a sorted list of these by Publisher, Author (last, first), then title</returns>
         [HttpGet("sorted")]
         public async Task<BookApiResponse> GetStortedBooksAsync()
         {
             return await _bookService.GetStortedBooksAsync();
         }
         /// <summary>
-        /// 
+        ///     Sort book list by Author (last, first) then title
         /// </summary>
-        /// <returns></returns>
+        /// <returns>return a sorted list by Author (last, first) then title</returns>
         [HttpGet("sortByAuthor")]
         public async Task<BookApiResponse> GetBooksSortedByAuthorAsync()
         {
             return await _bookService.GetBooksSortedByAuthorAsync();
         }
         /// <summary>
-        /// 
+        ///     Total Price of all books
         /// </summary>
-        /// <returns></returns>
+        /// <returns>return the total price of all books</returns>
         [HttpGet("totalPrice")]
         public async Task <BookApiResponse> GetTotalPriceOfBooksAsync()
         {
             return await _bookService.GetTotalPriceOfBooksAsync();
         }
         /// <summary>
-        /// 
+        ///     Save the entire list of books
         /// </summary>
         /// <param name="books"></param>
-        /// <returns></returns>
+        /// <returns>to save the entire list of books</returns>
         [HttpPost("saveBooks")]
         public async Task<BookApiResponse> SaveBooksAsync([FromBody]List<Book> books)
         {
--- BooksAPI/Utilities/SQLStrings.cs
+++ BooksAPI/Utilities/SQLStrings.cs
@@ -2,7 +2,7 @@
 {
     public static class SQLStrings
     {
-        public const string GetStortedBooks = "SELECT * FROM tbl_Books where IsActive=1 and IsDeleted=0 order by Publisher,AuthorLastName,AuthorFirstName,Title";
+        public const string GetStortedBooks = "SELECT * FROM tbl_Books where IsActive=1 and IsDeleted=0 order by Publisher,CONCAT(AuthorLastName, ' ', AuthorFirstName),Title";
         public const string GetBooksSortedByAuthor = "SELECT * FROM tbl_Books where IsActive=1 and IsDeleted=0 ORDER BY CONCAT(AuthorLastName, ' ', AuthorFirstName), Title";
         public const string GetTotalPriceOfBooks = "SELECT SUM(Price) FROM tbl_Books where IsActive=1 and IsDeleted=0 ";
         public const string SaveBooks = "INSERT INTO tbl_Books (Publisher, Title, AuthorLastName, AuthorFirstName, Price, IsActive, IsDeleted, CreatedDt, TitleContainer, Pages, PageRange, VolumeNo, IssueNo, URL, Language, Country) VALUES (@Publisher, @Title, @AuthorLastName, @AuthorFirstName, @Price, 1, 0, getdate(), @TitleContainer, @Pages, @PageRange, @VolumeNo, @IssueNo, @URL, @Language, @Country)";
